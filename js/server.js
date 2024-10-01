985b7cbb1d292ab6505fccb9d856f4e220257617
5GR5 <galrud5@gmail.com>
2024-10-01 14:52:59 +0000

    updates to the express server

--- js/server.js
+++ js/server.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5555;
 
 // Middleware
@@ -9,8 +10,31 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 // Basic route for home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/contacts page.html'); // Serve your HTML file
+    res.sendFile(path.join(__dirname, '../landing_page.html')); // Correctly serve your HTML file
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../cart.html')); // Correctly serve your HTML file
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../contacts page.html')); // Correctly serve your HTML file
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../admin_page.html')); // Correctly serve your HTML file
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../login page.html')); // Correctly serve your HTML file
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../personal_page.html')); // Correctly serve your HTML file
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../registration.html')); // Correctly serve your HTML file
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../result_page.html')); // Correctly serve your HTML file
+});
+
 
 // Example route for handling form submissions
 app.post('/submit', (req, res) => {
