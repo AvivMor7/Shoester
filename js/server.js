81762b4bf5720e69ec1e1acc935dce2b14f42aa1
5GR5 <galrud5@gmail.com>
2024-10-01 15:58:32 +0000

    the express server is working

--- js/server.js
+++ js/server.js
@@ -9,30 +9,35 @@ app.use(express.static('public')); // Serve static files
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 // Basic route for home page
-app.get('/', (req, res) => {
+
+app.get('/landing_page.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../landing_page.html')); // Correctly serve your HTML file
 });
 
-app.get('/', (req, res) => {
+app.get('/cart.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../cart.html')); // Correctly serve your HTML file
+
 });
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../contacts page.html')); // Correctly serve your HTML file
-});
-app.get('/', (req, res) => {
+app.get('/admin_page.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../admin_page.html')); // Correctly serve your HTML file
 });
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../login page.html')); // Correctly serve your HTML file
+app.get('/login_page.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../login_page.html')); // Correctly serve your HTML file
 });
-app.get('/', (req, res) => {
+app.get('/personal_page.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../personal_page.html')); // Correctly serve your HTML file
 });
-app.get('/', (req, res) => {
+app.get('/registration.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../registration.html')); // Correctly serve your HTML file
 });
-app.get('/', (req, res) => {
+app.get('/result_page.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../result_page.html')); // Correctly serve your HTML file
+});
+app.get('/contacts_page.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../contacts_page.html')); // Correctly serve your HTML file
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../landing_page.html')); // Correctly serve your HTML file
 });
 
 
